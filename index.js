570d70c7e439c2e0c6b2e1f42873e533151050cb
LaurenceJJones <Laurence.Jones@live.co.uk>
2020-05-04 09:24:32 +0000

    Initial commit basic commands added will need to work on added search headers

---
+++ index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const dbfstream = require('dbfstreammemo');
+const { program } = require('commander');
+program
+	.arguments('<filePath>')
+	.option('-hr, --header', 'Show Header Row')
+	.option('-nr, --numRows', 'Show Number Of Rows')
+	.option('-d, --showData', 'Show Data')
+	.option('-dr, --dataRow <number>', 'Show Data Row (Accept multiple rows split with ",")')
+	.option('-c, --column <name>', 'Show Column (Accept multiple params split with ",")')
+	.action((filePath, { header , showData , numRows }) => {
+		let obj = { data: []};
+		let dbf = dbfstream(filePath , 'utf-8');
+		let opts = program.column ? program.column.split(',') : false;
+		let rows = program.dataRow ? program.dataRow.split(',') : false;
+		dbf.on('header', (data) => {
+			obj.header = {...data}
+			if (numRows){ console.log(`Number Of Records: ${obj.header.numberOfRecords}`) };
+			if (header) { console.log(obj.header) };
+			if (!showData && !opts && !rows) { process.exit(0) }
+		});
+		dbf.on('data', (data) => {
+			obj.data.push(data);
+			if (rows){
+				rows.includes(data['@numOfRecord'].toString()) ? printRow(data, opts) : null;
+			}
+		});
+		dbf.on('end', () => {
+			if (showData && !opts) console.dir(obj.data);
+			if (showData && opts) {obj.data.forEach((obj) => {printRow(obj , opts)})}
+		});
+   	});
+function printRow(row , opts){
+	if (!opts){
+		let obj = {}
+		for (let [key, value] of Object.entries(row)) {
+  			obj[key] = value;
+		}
+		console.table(obj)
+	}else{
+		let obj = {
+			'Row' : row['@numOfRecord']
+		};
+		for (const word of opts){
+			for (let [key, value] of Object.entries(row)) {
+				if (key.toLowerCase().includes(word.toLowerCase())){
+					obj[key] = value;
+				}
+			}
+		}
+		console.table(obj)
+	}
+}
+program.parse(process.argv);
